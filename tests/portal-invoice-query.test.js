cc590bce4874a26988608f98247916d47dce0b86
Jessabel-Art <jsantos4@alumni.fullsail.edu>
2026-09-03 10:23:26 -0400

    Fix client portal invoice dashboard query

---
+++ tests/portal-invoice-query.test.js
@@ -0,0 +1,12 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import fs from "node:fs";
+
+const repositorySource = fs.readFileSync(
+  new URL("../server/repositories/portal-repository.php", import.meta.url),
+  "utf8",
+);
+
+test("portal invoice list does not query a non-existent stripe_sync_status column", () => {
+  assert.doesNotMatch(repositorySource, /i\.stripe_sync_status|stripe_sync_status/i);
+});
